7dd3e643024581da3b20f2074a1cbf3014925e27
Nasrullah Khan Ferdaus <nasrullah129261@gmail.com>
2025-12-11 07:20:44 +0600

    Create DS-1 lab.c

---
+++ DS-1 lab.c
@@ -0,0 +1,11 @@
+//write c programs to perform basic operations on arrays and measure performnce
+#include<stdio.h>
+int main(){
+    int a[]={1,2,3,4,5};
+    printf("the array is:");
+    for(int i=0;i<5;i++){
+        printf("%d",a[i]);
+    }
+    return 0;
+}
+
